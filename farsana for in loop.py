e626a63acc07b31432ebe134ce313c8912bef78c
farsanaanu <ashianuz816@gmail.com>
2024-07-15 11:39:20 +0530

    Add files via upload

---
+++ farsana for in loop.py
@@ -0,0 +1,42 @@
+input1=int(input('enter a value'))
+input1
+if (input1%5==0) & (input1%7==0):
+    print('divisible by 5and7')
+else:
+    print('non divisible by 5and7')
+if (input1%5==0) | (input1%7==0):
+    print('divisible by 5or7')
+else:
+    print('non divisible by 5or7')
+list1=[1,2,3,4,5,6,7,8,9,10,11,12]
+list1                    
+print(list1[0])
+print(list1[1])
+print(list1[2])
+print(list1[:])
+for value in list1:
+    print(value) 
+
+list(range(1,101))
+list2=range(1,101)
+list2
+for num in list2:
+    print(num)
+list(range(1,13))
+list1=range(1,13)
+for index in range(0,12):
+    print(list1[index])
+for index in range(0,len(list1)):
+    print(list1[index])
+for index in range(0,len(list1),2):
+    print(list1[index])
+for index in range(1,len(list1),2):
+    print(list1[index])
+tuple1=1,24,'anu',True,2.6
+tuple1
+for value in tuple1:
+    print(value)
+for value in range(0,len(tuple1)):
+    print(tuple1[value])
+
+ 
---
+++ farsana sets and if.py
@@ -0,0 +1,55 @@
+name=input('enter a value')
+print(name)
+name
+input2=int(input("enter a value"))
+input2
+type(input2)
+input3=float(input('enter a value'))
+input3
+bin(10)
+oct(10)
+hex(10)
+set1={7,5.6,'farsana',17,7,True,}
+set1
+type(set1)
+dir(set1)
+set2={48,7,'anu',100,5.6,False,48}
+set2
+type(set2)
+dir(set2)
+set1.intersection(set2)
+set1
+set1.difference(set2)
+set2.difference(set1)
+set1.union(set2)
+set1.symmetric_difference(set2)
+set1.add(100)
+set1
+x=4
+y=6
+print(x+y)
+print(x/y)
+print(x*y)
+print(x//y)
+print(x**y)
+range(1,10)
+range
+list(range(1,10))
+list(range(0,101,2))
+range1=list(range(0,101,2))
+range1
+len(range1)
+input5=float(input('enter a value'))
+input5
+input6=int(input('enter a value'))
+input6
+if input6%2==0:
+    print('even')
+else:
+    print('odd')
+if input6>50:
+    print('greaterthan 50')  
+elif input6<50:
+    print('lessthan 50')  
+else:
+    print('equal to 50')
