51479c86960e82b1ecbbd8a9b2b009b8da68427b
farsanaanu <ashianuz816@gmail.com>
2024-07-30 11:20:30 +0530

    Add files via upload

---
+++ numpy eg.py.fars.py
@@ -0,0 +1,25 @@
+import numpy as np
+list1=[1,2,3,4,5,6,34,23,11,6,8,9]
+list2=[6,'farsana',57,'me',8]
+a=np.array(list1)
+b=np.array(list2)
+type (a)
+type(b)
+a
+b
+dir(a)
+dir(b)
+a.min()
+a.max()
+a.mean()
+a.sum()
+a.size
+a.shape=(2,2,3)
+a.reshape(2,3)
+a.reshape(2,2,3)
+a
+
+
+
+
+
